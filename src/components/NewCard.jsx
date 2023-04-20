5123fd30645a407d42cfca9117adb2a2f63d92cd
SiberianCoffe <siberiancoffe@outlook.cl>
2023-04-19 23:54:03 -0400

    small improvements to news (needs more work)

--- src/components/NewCard.jsx
+++ src/components/NewCard.jsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 import Link from "@mui/material/Link";
 import { useDarkMode } from "../hooks/contex/DarkModeContex";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
@@ -10,6 +10,7 @@ import timezone from "dayjs/plugin/timezone";
 // icons
 import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+import PushPinRoundedIcon from "@mui/icons-material/PushPinRounded";
 //
 
 const NewCard = ({
@@ -18,6 +19,7 @@ const NewCard = ({
     content_summary,
     author,
     published_at,
+    pinned,
     url,
 }) => {
     const { darkMode, themeTatailwind } = useDarkMode();
@@ -33,58 +35,67 @@ const NewCard = ({
     };
 
     return (
-        <div
-            className={`flex flex-col ${themeTatailwind.secundary.main} rounded-lg border-2 border-transparent hover:border-[#3ca9d0] shadow-2xl m-4 mb-12 p-4`}
-        >
-            <img
-                className="object-cover rounded-lg drop-shadow-lg"
-                src="https://static.truckersmp.com/images/event/cover/14653.1678403142.jpeg"
-                alt="Los Andes VTC logo"
-            />
-            <div className="flex flex-col h-full pb-2">
-                <div>
-                    <Link
-                        className="flex justify-center p-2"
-                        href={url}
-                        color={color}
-                        target="_blank"
-                        underline="none"
-                    >
-                        <Typography variant="h6">{title}</Typography>
-                    </Link>
-                    <Divider />
+        <div className="flex relative">
+            {pinned ? (
+                <div className="flex absolute -rotate-45">
+                    <PushPinRoundedIcon color={"info"} fontSize={"large"} />
                 </div>
-                <div className="flex pt-2">
-                    <Typography
-                        component={"div"}
-                        color={color}
-                        variant="subtitle2"
-                    >
-                        <div>{content_summary}</div>
-                    </Typography>
-                </div>
-                <div className="flex h-full w-full pt-3">
-                    <Typography
-                        component={"div"}
-                        className="flex h-full w-full"
-                        color={color}
-                        variant="caption"
-                    >
-                        <div className="flex w-full items-end">
-                            <PersonRoundedIcon
-                                sx={{ width: 21, height: 21 }}
-                                className="mr-2"
-                            />
-                            {author}
-                        </div>
-                        <div className="flex w-full items-end justify-end">
-                            <CalendarMonthRoundedIcon
-                                sx={{ width: 21, height: 21 }}
-                                className="mr-2"
-                            />
-                            {formatDate(published_at)}
-                        </div>
-                    </Typography>
+            ) : (
+                <></>
+            )}
+            <div
+                className={`flex flex-col ${themeTatailwind.secundary.main} rounded-lg border-2 border-transparent hover:border-[#3ca9d0] shadow-2xl m-4 mb-12 p-4`}
+            >
+                <img
+                    className="object-cover rounded-lg drop-shadow-lg"
+                    src="https://static.truckersmp.com/images/event/cover/14653.1678403142.jpeg"
+                    alt="Los Andes VTC logo"
+                />
+                <div className="flex flex-col h-full pb-2">
+                    <div>
+                        <Link
+                            className="flex justify-center p-2"
+                            href={url}
+                            color={color}
+                            target="_blank"
+                            underline="none"
+                        >
+                            <Typography variant="h6">{title}</Typography>
+                        </Link>
+                        <Divider />
+                    </div>
+                    <div className="flex pt-2">
+                        <Typography
+                            component={"div"}
+                            color={color}
+                            variant="subtitle2"
+                        >
+                            <div>{content_summary}</div>
+                        </Typography>
+                    </div>
+                    <div className="flex h-full w-full pt-3">
+                        <Typography
+                            component={"div"}
+                            className="flex h-full w-full"
+                            color={color}
+                            variant="caption"
+                        >
+                            <div className="flex w-full items-end">
+                                <PersonRoundedIcon
+                                    sx={{ width: 21, height: 21 }}
+                                    className="mr-2"
+                                />
+                                {author}
+                            </div>
+                            <div className="flex w-full items-end justify-end">
+                                <CalendarMonthRoundedIcon
+                                    sx={{ width: 21, height: 21 }}
+                                    className="mr-2"
+                                />
+                                {formatDate(published_at)}
+                            </div>
+                        </Typography>
+                    </div>
                 </div>
             </div>
         </div>
--- src/routes/pages/News.jsx
+++ src/routes/pages/News.jsx
@@ -2,6 +2,12 @@ import { Typography } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import NewCard from "../../components/NewCard";
 import useFetch from "../../hooks/useFetch";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { useDarkMode } from "../../hooks/contex/DarkModeContex";
+import EmptyData from "../../components/EmptyData";
+import ErrorData from "../../components/ErrorData";
+// import PaginationItem from "@mui/material/PaginationItem";
 
 import {
     PROXY_CORS_URL_GET,
@@ -11,7 +17,11 @@ import {
 
 const News = () => {
     const loaded = useRef(false);
+    const firstRender = useRef(true);
     const [NewsResponse, setNewsResponse] = useState(false);
+    const { darkMode } = useDarkMode();
+
+    const color = darkMode ? "white" : "black";
 
     // eslint-disable-next-line
     const [loading, error, succes, bodyResponse] = useFetch(
@@ -36,6 +46,7 @@ const News = () => {
     };
 
     useEffect(() => {
+        firstRender.current = false;
         if (!loaded.current) {
             getNews();
             loaded.current = true;
@@ -44,8 +55,11 @@ const News = () => {
 
     const renderNews = () => {
         if (NewsResponse && NewsResponse.length > 0) {
-            console.log(NewsResponse);
-            return NewsResponse.map((event) => {
+            let newListCopy = JSON.parse(JSON.stringify(NewsResponse));
+            newListCopy.sort((a, b) => {
+                return new Date(b.published_at) - new Date(a.published_at);
+            });
+            return newListCopy.map((event) => {
                 return (
                     <NewCard
                         key={event.id}
@@ -53,19 +67,97 @@ const News = () => {
                         content_summary={event.content_summary}
                         author={event.author}
                         published_at={event.published_at}
+                        pinned={event.pinned}
                         url={`https://truckersmp.com/`}
                     />
                 );
             });
         }
-        return <Typography>No hay eventos próximos</Typography>;
+        return <EmptyData msj={"No se encontraron noticias"} />;
+    };
+
+    const renderPinnedNews = () => {
+        if (NewsResponse && NewsResponse.length > 0) {
+            let newListCopy = JSON.parse(JSON.stringify(NewsResponse));
+            const pinnedNews = newListCopy.filter((event) => {
+                return event.pinned;
+            });
+            if (pinnedNews.length > 0) {
+                return pinnedNews.map((event) => {
+                    return (
+                        <NewCard
+                            key={event.id}
+                            title={event.title}
+                            content_summary={event.content_summary}
+                            author={event.author}
+                            published_at={event.published_at}
+                            pinned={event.pinned}
+                            url={`https://truckersmp.com/`}
+                        />
+                    );
+                });
+            }
+            return <EmptyData msj={"No se encontraron noticias destacadas"} />;
+        }
+        return <EmptyData msj={"No se encontraron noticias destacadas"} />;
+    };
+
+    const renderPage = () => {
+        return (
+            <>
+                <div className="flex flex-col rounded-lg border-2 border-yellow-500 md:m-4">
+                    <Typography
+                        className="flex justify-center pb-4 pt-4"
+                        component={"div"}
+                        color={color}
+                        variant="h6"
+                    >
+                        Destacados
+                    </Typography>
+                    <div className="grid md:grid-cols-3">
+                        {renderPinnedNews()}
+                    </div>
+                </div>
+                <Typography
+                    className="flex justify-center pb-4 pt-4"
+                    component={"div"}
+                    color={color}
+                    variant="h6"
+                >
+                    Lo ultimo
+                </Typography>
+                <div className="grid md:grid-cols-3">{renderNews()}</div>
+                <div className="flex justify-center pb-5">
+                    <Stack spacing={2}>
+                        <Pagination
+                            count={10}
+                            variant="outlined"
+                            shape="rounded"
+                        />
+                    </Stack>
+                </div>
+            </>
+        );
+    };
+
+    const checkError = () => {
+        if (error) {
+            return <ErrorData msj={"Error al cargar las noticias"} />;
+        } else if (!loading && firstRender.current === false) {
+            return renderPage();
+        }
     };
 
     return (
-        <div className="grid md:grid-cols-3">
+        <>
+            <div className="flex justify-center m-2">
+                <Typography color={color} variant="h4">
+                    Noticias
+                </Typography>
+            </div>
             {loading && <Typography variant="h4">Loading...</Typography>}
-            {!error && !loading ? renderNews() : <></>}
-        </div>
+            {checkError()}
+        </>
     );
 };
 
