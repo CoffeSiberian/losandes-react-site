087b5aab1f7cc050613c616d6b44ae527f09a78a
SiberianCoffe <siberiancoffe@outlook.cl>
2023-05-09 20:42:12 -0400

    the content was structured

--- src/routes/pages/HallOfFame.jsx
+++ src/routes/pages/HallOfFame.jsx
@@ -37,28 +37,52 @@ const HallOfFame = () => {
     }, []);
 
     const renderMembers = (rol_id, rol_name) => {
-        if (!Response || Response.length === 0) return <EmptyData />;
+        if ((!Response || Response.length === 0) && !loading) {
+            return <EmptyData key={rol_id + "empydata"} msj="Sin resultados" />;
+        }
         const membersFilter = Response.filter((member) =>
             member.roles.includes(rol_id)
         );
         if (membersFilter.length === 0) return <></>;
-        return membersFilter.map((member) => {
-            return (
-                <MemberCard
-                    key={member.user.id}
-                    id={member.user.id}
-                    username={member.nick}
-                    roleName={rol_name}
-                    imgLink={`${cdnDiscordAvatar}${member.user.id}/${member.user.avatar}`}
-                    dateJoin={member.joined_at}
-                />
-            );
-        });
+        return (
+            <div
+                className={`flex flex-col rounded-lg border-2 border-cyan-600 md:ml-4 md:mr-4`}
+                key={rol_id}
+            >
+                <Typography
+                    component="div"
+                    key={rol_id + "name"}
+                    className="flex justify-center pt-3"
+                    variant="h5"
+                    color={themeTatailwind.primary.color}
+                >
+                    <b>{rol_name}</b>
+                </Typography>
+
+                <div
+                    key={rol_id + "cards"}
+                    className="flex flex-col items-center w-full gap-5 p-6"
+                >
+                    {membersFilter.map((member) => {
+                        return (
+                            <MemberCard
+                                key={rol_id + "card"}
+                                id={member.user.id}
+                                username={member.nick}
+                                roleName={rol_name}
+                                imgLink={`${cdnDiscordAvatar}${member.user.id}/${member.user.avatar}`}
+                                dateJoin={member.joined_at}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        );
     };
 
     const renderPage = () => {
         return (
-            <div key="contenHall" className="grid md:grid-cols-3">
+            <div key="contenHall" className="flex flex-col mb-5 gap-5">
                 {HALL_OF_FAME.map((rol) => {
                     return renderMembers(rol.rol_id, rol.rol_name);
                 })}
