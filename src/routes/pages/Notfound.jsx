3c598aed4cc813d7a37a7288c065688674b03aeb
Fernando Garrido <siberiancoffe@outlook.cl>
2023-05-25 12:43:47 -0400

    warning to non-existent routes

--- src/routes/pages/Notfound.jsx
+++ src/routes/pages/Notfound.jsx
@@ -1,7 +1,9 @@
 import { TITLE } from "../../helpers/configs";
+import EmptyData from "../../components/EmptyData";
+
 const Notfound = () => {
-    document.title = TITLE + " | Not Found";
-    return <div>Notfound</div>;
+    document.title = TITLE + " | 404";
+    return <EmptyData msj={"No se encontró la página que buscas"} />;
 };
 
 export default Notfound;
