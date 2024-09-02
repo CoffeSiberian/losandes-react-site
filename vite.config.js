762ff00efb2f6e93a5af90a6aa5b852375024374
Fernando Garrido <siberiancoffe@outlook.cl>
2024-09-02 12:08:21 -0400

    chore: Update package dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
   server: {
     open: true,
   },
