b64402a5eb0f323fe1367224f817298f34fef170
Fernando Garrido <siberiancoffe@outlook.cl>
2024-09-13 16:15:39 -0300

    chore: Update package dependencies and build script in package.json

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,21 @@ export default defineConfig({
   server: {
     open: true,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            const modules = id.split("node_modules/")[1].split("/")[0];
+            return `vendor-${modules}`;
+          }
+          if (id.includes("src")) {
+            const parts = id.split("src/")[1].split("/");
+            return parts.length > 1 ? parts[0] : "main";
+          }
+        },
+      },
+    },
+  },
   plugins: [react()]
 })
